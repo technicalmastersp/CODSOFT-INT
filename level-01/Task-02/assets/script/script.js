2b87e235268babcd31cb0ee1cbc602d93506e2da
technicalmastersp <takebestservices@gmail.com>
2024-09-01 12:27:14 +0530

    add this script file for sume additional function code here

---
+++ level-01/Task-02/assets/script/script.js
@@ -0,0 +1,8 @@
+// alert("Hii...")
+
+// let menuTab = document.querySelector('.material-symbols-outlined').innerText
+// let menuTabT = document.querySelector('.material-symbols-outlined').innerText
+
+// console.log(menuTab)
+// console.log("hello")
+// console.log(menuTabT)
